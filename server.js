cdf35a73189675112b768423c58178057b318451
vasil <vasildimitrovhristov@gmail.com>
2018-10-15 08:13:15 +0300

    Deployment configuration and package.json changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require("body-parser")
 const passport = require('passport')
+const path = require('path')
 
 // routes
 const users = require('./routes/api/users')
@@ -33,6 +34,13 @@ app.use('/api/users/', users)
 app.use('/api/profile/', profile)
 app.use('/api/posts/', posts)
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
